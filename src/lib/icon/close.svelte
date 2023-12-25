d0c2f734c2a84c9533e12f0c6cd014cc9a017661
Simon Kurz <simonlukas.98@gmail.com>
2023-12-25 12:16:14 +0100

    Add icon wrapper to close and search icons

--- src/lib/icon/close.svelte
+++ src/lib/icon/close.svelte
@@ -1,10 +1,18 @@
-<svg
-	xmlns="http://www.w3.org/2000/svg"
-	fill="none"
-	viewBox="0 0 24 24"
-	stroke-width="1.5"
-	stroke="currentColor"
-	class="w-6 h-6"
->
-	<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-</svg>
+<script lang="ts">
+	import Icon from './icon.svelte';
+	import type { Size } from './icon.svelte';
+
+	export let size: Size | number | undefined = undefined;
+</script>
+
+<Icon {size}>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+	>
+		<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+	</svg>
+</Icon>
