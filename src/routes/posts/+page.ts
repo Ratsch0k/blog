f8ebfdd029b9a7328e6c09954d9f789f722682a4
Simon Kurz <simonlukas.98@gmail.com>
2024-07-04 18:57:34 +0200

    Change order of all posts from newest to oldest

--- src/routes/posts/+page.ts
+++ src/routes/posts/+page.ts
@@ -5,15 +5,16 @@ export const load: PageLoad = async () => {
 	const allPosts = await import.meta.glob('../../content/posts/**/*.{svelte,md,svx}');
 
 	const posts: Post[] = await Promise.all(
-		Object.entries(allPosts).map(async ([path, resolver]) => {
-			const { default: page, metadata } = await resolver();
-			const slug = path.replace('../../content/posts', '/post').replace('index.md', '');
+		Object.entries(allPosts)
+			.map(async ([path, resolver]) => {
+				const { default: page, metadata } = await resolver();
+				const slug = path.replace('../../content/posts', '/post').replace('index.md', '');
 
-			return toPost({ page, metadata, path, slug });
-		})
+				return toPost({ page, metadata, path, slug });
+			})
 	);
 
-	posts.sort((a, b) => (a.publishedAt < b.publishedAt ? -1 : 1));
+	posts.sort((a, b) => (a.publishedAt > b.publishedAt ? -1 : 1));
 
 	return { posts };
 };
